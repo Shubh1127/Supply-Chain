629085c66364bad18807dfaad9749796fa379faa
shubham <shubhamsinghmor2312@gmail.com>
2025-01-23 23:24:00 +0530

    done

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,10 @@ const cookieParser = require('cookie-parser');
 const connectDB = require('./db/db');
 const cors=require('cors');
 require('dotenv').config();
-app.use(cors({origin:'http://localhost:5173',credentials:true}));
+app.use(cors({
+    origin: 'https://supply-chain-1.onrender.com', 
+    credentials: true
+  }));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
