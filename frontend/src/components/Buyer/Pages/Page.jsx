3e94918b721a2a976f68268ece8007ae7fe57d9d
shubham <shubhamsinghmor2312@gmail.com>
2025-01-23 23:10:35 +0530

    done

--- frontend/src/components/Buyer/Pages/Page.jsx
+++ frontend/src/components/Buyer/Pages/Page.jsx
@@ -1,6 +1,6 @@
 import Header from '../components/BuyerHeader'
 import Hero from '../components/BuyerHero'
-import ProductCategories from '../Components/ProductCategories'
+import ProductCategories from '../components/ProductCategories'
 import RecommendedProducts from '../Components/RecommendedProducts'
 import FeaturedProducts from '../components/FeaturedProducts'
 import Footer from '../components/BuyerFooter'
