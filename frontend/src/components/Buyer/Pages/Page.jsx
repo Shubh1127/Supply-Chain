c8eb75e4f1a99f26f206ac15cdc608c39d991c3a
shubham <shubhamsinghmor2312@gmail.com>
2025-01-23 23:12:47 +0530

    done

--- frontend/src/components/Buyer/Pages/Page.jsx
+++ frontend/src/components/Buyer/Pages/Page.jsx
@@ -1,7 +1,7 @@
 import Header from '../components/BuyerHeader'
 import Hero from '../components/BuyerHero'
 import ProductCategories from '../components/ProductCategories'
-import RecommendedProducts from '../Components/RecommendedProducts'
+import RecommendedProducts from '../components/RecommendedProducts'
 import FeaturedProducts from '../components/FeaturedProducts'
 import Footer from '../components/BuyerFooter'
 
